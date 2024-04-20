d7d88df030f2f06b8ed7d458a5136ea996468520
Черепахин Василий <vasi.che@gmail.com>
2024-04-20 23:18:24 +0500

    add VacancyList.propTypes

--- src/components/VacancyList.js
+++ src/components/VacancyList.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { ListGroup } from "react-bootstrap";
+import PropTypes from "prop-types";
+
 import Vacancy from './Vacancy';
 
 const VacancyList = ({ vacancies }) => {
@@ -15,10 +17,13 @@ const VacancyList = ({ vacancies }) => {
   else return null;
 }
 
-//TODO: Add PropTypes for vacancies array
-//VacancyList.propTypes = {
-//    vacancies: React.PropTypes.arrayOf(React.PropTypes.object)
-//}
+VacancyList.propTypes = {
+    vacancies: PropTypes.arrayOf(PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            title: PropTypes.string.isRequired,
+            completed: PropTypes.bool.isRequired
+    }))
+}
 
 VacancyList.defaultProps = {
     vacancies: [],

65e0c5e68591a02220e4a0c76e60bd60445276f1
Черепахин Василий <vasi.che@gmail.com>
2024-04-29 23:46:44 +0500

    width buttons with &nbsp;

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
     <Container fluid className="md-0 pt-0 main-app-container bg-light"> {/*  "ml-4" Margin Left (ml!) (https://react-bootstrap.github.io/docs/layout/grid)*/}
       <Row className="fixed-top mt-0 md-1 pd-1 pl-0 ml-0 bg-light table-bordered">
         <Col xs={12} className="mt-2 mb-2">
-            <Button className="ml-0 col-2">Новая</Button>
-            <Button className="ml-1 col-2">Все</Button>
-            <Button className="ml-1 col-2">В работе</Button>
+            <Button className="ml-0">&nbsp;&nbsp;Новая&nbsp;&nbsp;&nbsp;</Button>
+            <Button className="ml-1">&nbsp;&nbsp;&nbsp;&nbsp;Все&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</Button>
+            <Button className="ml-1">В работе</Button>
         </Col>
       </Row>
       <Row className="mt-2 md-0 pt-5 pl-0 pd-0">    {/* "mt-4"  top margin (https://react-bootstrap.github.io/docs/layout/grid)*/}

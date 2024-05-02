d2b6e3b752d0102123d0fbb3f818c16392a93623
Черепахин Василий <vasi.che@gmail.com>
2024-05-02 23:30:22 +0500

    extract NewVacancyPanel.js

---
+++ src/components/NewVacancyPanel.spec.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
+import NewVacancyPanel from "./NewVacancyPanel";
+
+describe("<NewVacancyPanel />", () => {
+
+  it("ClassName NewVacancyPanel area", () => {
+    const wrapper = shallow(<NewVacancyPanel />);
+    // for demo logging props:
+    //    const props = wrapper.props();
+    //    console.log(wrapper.debug());
+    //    console.log(wrapper.props().children);
+    //    console.log(wrapper.props().children[1].props);
+
+    const panelProps = wrapper.props();
+    expect(panelProps.className).toEqual('mt-2 md-0 pt-5 pl-0 pd-0');
+  });
+
+});

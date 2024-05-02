561c0f65a12a0e0bfad131e26238ded7713eb813
Черепахин Василий <vasi.che@gmail.com>
2024-05-02 16:33:46 +0500

    add it("Size column ToolBar of App is 12 columns)

--- src/App.Toolbar.shallow.spec.js
+++ src/App.Toolbar.shallow.spec.js
@@ -12,23 +12,23 @@ describe("<App Toolbar />", () => {
 
   it("ClassName ToolBar of App", () => {
     const wrapper = shallow(<App />);
-//    const props = wrapper.props();
-//    console.log(wrapper.debug());
-//    console.log(wrapper.props().children);
-//    console.log(wrapper.props().children[1].props);
+    // for demo shop props:
+    //    const props = wrapper.props();
+    //    console.log(wrapper.debug());
+    //    console.log(wrapper.props().children);
+    //    console.log(wrapper.props().children[1].props);
 
     const toolBarProps = wrapper.props().children[1].props;
     expect(toolBarProps.className).toEqual('mt-2 md-0 pt-5 pl-0 pd-0');
   });
 
-  it("Size ToolBar of App", () => {
+  it("Size column ToolBar of App is 12 columns", () => {
     const wrapper = shallow(<App />);
     const props = wrapper.props();
     // children[3] - Container - 1, Row(toolbar) - 2, Col (toolbar) - 3
-    expect(wrapper.props().children[3].props.children.props.xs).toEqual(12);
-    // for demo
-    const toolBarProps = wrapper.props().children[3].props;
-    expect(wrapper.props().children[3].props.children.props.xs).toEqual(12);
+    const columnToolBarProps = wrapper.props().children[3].props;
+    // toolbar column <Col xs={12}>
+    expect(columnToolBarProps.children.props.xs).toEqual(12);
 
   });
 

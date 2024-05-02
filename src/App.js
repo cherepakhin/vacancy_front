2e5dcd11f5d2fc7b33e1892423bf1f3269b40c18
Черепахин Василий <vasi.che@gmail.com>
2024-05-02 23:13:38 +0500

    extract NewVacancyPanel.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Container, Col, Row } from "react-bootstrap";
 import VacancyList from "./components/VacancyList";
-import AddNewVacancy from "./components/AddNewVacancy";
 import MainToolbar from "./components/MainToolbar";
+import NewVacancyPanel from "./components/NewVacancyPanel";
 import { useSelector } from "react-redux";
 import './App.css';
 
@@ -15,13 +15,7 @@ function App() {
     // this is comment OUTside Component. For comment INside Component see below.
     <Container fluid className="md-0 pt-0 main-app-container bg-light">
       <MainToolbar />
-      {/*TODO: для ввода вакансий нужна не просто строка, а отдельный НЕ МОДАЛЬНЫЙ диалог (в модальный не влезет). */}
-      <Row className="mt-2 md-0 pt-5 pl-0 pd-0">    {/* "mt-4"  top margin (https://react-bootstrap.github.io/docs/layout/grid)*/}
-        <Col xs={6}>           {/* xs={6} 10 cells (https://react-bootstrap.github.io/docs/layout/grid)*/}
-          <h4>Новая вакансия</h4>
-          <AddNewVacancy />
-        </Col>
-      </Row>
+      <NewVacancyPanel />
       <Row className="mt-0 pt-0">
         <Col>
           <h4 id="label_list_vacancies">Список вакансий</h4>
---
+++ src/components/NewVacancyPanel.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Col, Row } from "react-bootstrap";
+import AddNewVacancy from "./AddNewVacancy";
+
+// TODO: для ввода вакансий нужна не просто строка, а отдельный НЕ МОДАЛЬНЫЙ диалог (в модальный не влезет).
+const NewVacancyPanel = () => {
+    return (
+      <Row className="mt-2 md-0 pt-5 pl-0 pd-0">    {/* "mt-4"  top margin (https://react-bootstrap.github.io/docs/layout/grid)*/}
+        <Col xs={6}>           {/* xs={6} 10 cells (https://react-bootstrap.github.io/docs/layout/grid)*/}
+          <h4>Новая вакансия</h4>
+          <AddNewVacancy />
+        </Col>
+      </Row>
+    )
+}
+
+export default NewVacancyPanel;

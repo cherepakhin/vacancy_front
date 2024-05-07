9f77593dc34e21ed343fe3e598fb0070adf478c1
Черепахин Василий <vasi.che@gmail.com>
2024-05-07 22:30:34 +0500

    add comment for hide panel

--- src/components/NewVacancyPanel.js
+++ src/components/NewVacancyPanel.js
@@ -6,7 +6,8 @@ import AddNewVacancy from "./vacancies/AddNewVacancy";
 const NewVacancyPanel = () => {
     return (
       <Row className="mt-2 md-0 pt-5 pl-0 pd-0">    {/* "mt-4"  top margin (https://react-bootstrap.github.io/docs/layout/grid)*/}
-        <Col xs={6}>           {/* xs={6} 10 cells (https://react-bootstrap.github.io/docs/layout/grid)*/}
+        {/* <Col xs={6} className="d-none">            add className="d-none" for hide*/}
+        <Col xs={6}>           {/* add className="d-none" for hide, xs={6} 10 cells (https://react-bootstrap.github.io/docs/layout/grid)*/}
           <h4>Новая вакансия</h4>
           <AddNewVacancy />
         </Col>

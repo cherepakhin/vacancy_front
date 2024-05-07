f741303d397c995d0ce521e16614d232c57e6792
Черепахин Василий <vasi.che@gmail.com>
2024-05-07 11:47:53 +0500

    work add vacansy with redux

--- src/App.js
+++ src/App.js
@@ -19,24 +19,24 @@ class App extends React.Component { // receives props from index.js
       console.log("App.render() this:");
       console.log(this);
       console.log("App.render() this.props.vacancies:");
-      console.log(this.props.vacancies);
+      console.log(this.props.props.vacancies);
       return (
         //  "md-0" Margin Down (mD!) (https://react-bootstrap.github.io/docs/layout/grid)
         // this is comment OUTside Component. For comment INside Component see below.
         <Container fluid className="md-0 pt-0 main-app-container bg-light">
           <MainToolbar />
           <NewVacancyPanel /> {/* TODO: remove to dialog for new vacancy */}
-          <Vacancies vacancies={this.props.vacancies} />  {/* TODO: remove to dialog for new vacancy */}
+          <Vacancies vacancies={this.props.props.vacancies} />  {/* TODO: remove to dialog for new vacancy */}
         </Container>
       );
   }
 }
 
 function mapStateToProps(state) { // перенос из state в this.props....
-    console.log("App. mapStateToProps:"+JSON.stringify(state.vacancies)); // App. mapStateToProps:{"vacancies":[{"completed":false,"id":-1,"title":"не показывать -1"}]}
-    console.log("App. state.vacancies:"+JSON.stringify(state.vacancies)); // App. state.vacancies:[{"completed":false,"id":-1,"title":"не показывать -1"}]
+    console.log("App. mapStateToProps:"+JSON.stringify(state)); // App. mapStateToProps:{"vacancies":[{"completed":false,"id":-1,"title":"не показывать -1"}]}
+    console.log("App. state.vacancies:"+JSON.stringify(state)); // App. state.vacancies:[{"completed":false,"id":-1,"title":"не показывать -1"}]
     return {
-        vacancies: state.vacancies // "vacanciesFromState:" внутри компонета доступно через this.props.vacancies
+        props: state // "vacanciesFromState:" внутри компонета доступно через this.props.vacancies
     }
 }
 

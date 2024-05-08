ba8ab2185b13a247b2e5477b78ba5f001817378e
Черепахин Василий <vasi.che@gmail.com>
2024-05-08 08:44:24 +0500

    add comment for style

--- src/components/MainToolbar.js
+++ src/components/MainToolbar.js
@@ -3,16 +3,17 @@ import { Col, Row, Button } from "react-bootstrap";
 
 const MainToolbar = () => {
     return (
+      {/* className="fixed-top" - row fixed on TOP PAGE  */}
+      {/* "mt-0 md-1 ml-0" - margin top, margin bottom, margin left */}
+      {/* "pd-1 pl-0" - padding down, padding left */}
       <Row className="fixed-top mt-0 md-1 pd-1 pl-0 ml-0 bg-light table-bordered">
-      {/* Toolbar (comment INside Component)*/}
-      {/* Row fixed on TOP PAGE (fixed-top) */}
-        {/* Одна колонка во всю ширину экрана (xs=12).*/}
+        {/* xs={12} одна колонка во всю ширину экрана(12). */}
         {/* Отступ элементов внутри колонки сверху(mt-2) и снизу (mb-2) = 8px */}
         <Col xs={12} className="mt-2 mb-2">
-            {/* все кнопки имеют одинаковую ширину "col-md-1 col-sm-2" */}
-            {/* on middle device 1 column(col-md-1),*/}
-            {/* on small device 2 columns(col-sm-2)*/}
-            {/* отступ элементов внутри колонки mr-1. margin right = 1 (1 char 'x') */}
+            {/* col-md-1 col-sm-2 все кнопки имеют одинаковую ширину */}
+            {/* col-md-1 width on middle device = 1 column,*/}
+            {/* col-sm-2 width on small device = 2 columns*/}
+            {/* mr-1 отступ справа внутри колонки margin right = 1 (1 char 'x') */}
             <Button className="mr-1 col-md-1 col-sm-2">Все</Button>
             <Button className="mr-1 col-md-1 col-sm-2">Текущие</Button>
             <Button className="mr-1 col-md-1 col-sm-2">Новая</Button>

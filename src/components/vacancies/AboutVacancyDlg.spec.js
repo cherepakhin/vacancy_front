4163f21ee4e606f72f0edb630134f8eae2e4bc02
Черепахин Василий <vasi.che@gmail.com>
2024-05-14 15:19:27 +0500

    test AboutVacancyDlg.spec.js props show, hide, visible, modal,

--- src/components/vacancies/AboutVacancyDlg.spec.js
+++ src/components/vacancies/AboutVacancyDlg.spec.js
@@ -12,22 +12,61 @@ jest.mock('react-redux', () => ({
 }));
 
 describe("AboutVacancyDlg test", () => {
-  it("AboutVacancyDlg check structure with MOUNT", () => {
+  it("AboutVacancyDlg check prop vacancy with MOUNT", () => {
     const vacancy = {
       id: 100,
       title: "Vacancy 1"
     };
-//      const wrapper = shallow(<DeleteConfirmDlg id='100' title="Vacancy 1"/>);
     const wrapper = mount(<AboutVacancyDlg vacancy={{id: 100, title: "Vacancy 1"}} visible={true} fnClose={() => jest.fn()}/>);
     const dlg = toJson(wrapper);
     // demo log
     //    console.log(dlg);
     //    console.log(dlg.node.props);
-    expect(dlg.children[0].type).toBe("Modal");
-    expect(dlg.children[0].props.show).toBe(true);
 //    expect(dlg.children[0].props.className).toBe("rounded-0");
 
     expect(dlg.node.props.vacancy).toEqual(vacancy);
+  });
 
+  it("AboutVacancyDlg check prop visible with MOUNT", () => {
+    const vacancy = {
+      id: 100,
+      title: "Vacancy 1"
+    };
+    const wrapper = mount(<AboutVacancyDlg vacancy={{id: 100, title: "Vacancy 1"}} visible={true} fnClose={() => jest.fn()}/>);
+    const dlg = toJson(wrapper);
+
+    expect(dlg.node.props.visible).toEqual(true);
+  });
+
+  it("AboutVacancyDlg check is modal", () => {
+    const vacancy = {
+      id: 100,
+      title: "Vacancy 1"
+    };
+    const wrapper = mount(<AboutVacancyDlg vacancy={{id: 100, title: "Vacancy 1"}} visible={true} fnClose={() => jest.fn()}/>);
+    const dlg = toJson(wrapper);
+    expect(dlg.children[0].type).toBe("Modal");
+  });
+
+  it("AboutVacancyDlg check show TRUE", () => {
+    const vacancy = {
+      id: 100,
+      title: "Vacancy 1"
+    };
+    const wrapper = mount(<AboutVacancyDlg vacancy={{id: 100, title: "Vacancy 1"}} visible={true} fnClose={() => jest.fn()}/>);
+    const dlg = toJson(wrapper);
+
+    expect(dlg.children[0].props.show).toBe(true);
+  });
+
+  it("AboutVacancyDlg check show FALSE", () => {
+    const vacancy = {
+      id: 100,
+      title: "Vacancy 1"
+    };
+    const wrapper = mount(<AboutVacancyDlg vacancy={{id: 100, title: "Vacancy 1"}} visible={false} fnClose={() => jest.fn()}/>);
+    const dlg = toJson(wrapper);
+
+    expect(dlg.children[0].props.show).toBe(false);
   });
 });

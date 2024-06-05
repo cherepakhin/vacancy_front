79bfaa35fc1a22677058fe0be12050a6450dbe73
Черепахин Василий <vasi.che@gmail.com>
2024-06-05 19:03:16 +0500

    Эксперименты с div выравниванием

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends React.Component { // receives props from index.js
         // Attention:  access to props with __THIS.props.*__
         // <NewVacancyPanel visible={this.props.visibleNewVacancyDlg}/>
         <div>
-          <div className="button-div mr-1 col-md-1 col-sm-1">Button11</div>
+          <div className="button-div mr-1 col-md-1 col-sm-1">Button1</div>
           <div className="button-div mr-1 col-md-1 col-sm-1">12456</div>
           <div className="button-div mr-1 col-md-1 col-sm-1">123</div>
         </div>
